0a72d3e76ee9bf75253766a9d94b4676089ffd74
Huzeyge <huzeyfebostan99@gmail.com>
2022-01-01 00:28:50 +0300

    first push

--- 1-9/Problem03.c
+++ 1-9/Problem03.c
@@ -12,10 +12,10 @@ int main(void)
   unsigned long long n = 600851475143ULL;
   unsigned long long i;
 
-  for (i = 2ULL; i < n; i++) {
-    while (n % i == 0) {
+  for (i = 2ULL; i < n; i++) 
+  {
+    while (n % i == 0)
       n /= i;
-    }
   }
   printf("%llu\n", n);
 
